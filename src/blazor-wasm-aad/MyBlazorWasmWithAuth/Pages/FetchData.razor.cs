55fff60c1c7811cfc79c63a8775b750601bbf51d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-03-31 00:30:29 -0400

    changes to call weather api

---
+++ src/blazor-wasm-aad/MyBlazorWasmWithAuth/Pages/FetchData.razor.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using MyBlazorWasmWithAuth;
+using MyBlazorWasmWithAuth.Shared;
+using MyBlazorWasmWithAuth.Components;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MyBlazorWasmWithAuth.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+        private WeatherData? weatherData;
+        protected override async Task OnInitializedAsync()
+        {
+            // Las Vegas
+            var lat = "36.16";
+            var lon = "115.13";
+            weatherData = await Http.GetFromJsonAsync<WeatherData>(@$"https://www.7timer.info/bin/api.pl?lon={lon}&lat={lat}&product=civillight&output=json");
+            
+            forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+
+        public class WeatherForecast
+        {
+            public DateTime Date { get; set; }
+            public int TemperatureC { get; set; }
+
+            public string? Summary { get; set; }
+
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+
+    public class WeatherData {
+        public string? Product { get; set; }
+        
+        [JsonPropertyName("init")]
+        public string? Date { get; set; }
+        
+        [JsonPropertyName("dataseries")]
+        public List<WeatherDetails>? Forecasts { get;set;}
+    }
+    public class WeatherDetails
+    {
+        [JsonPropertyName("date")]
+        public int DateInt { get; set; }
+        
+        [JsonPropertyName("weather")]
+        public string? Summary { get; set; }
+        
+        [JsonPropertyName("temp2m")]
+        public Temp? Temp { get; set; }
+        
+        [JsonPropertyName("wind10m_max")]
+        public int Wind { get; set; }
+    }
+    public class Temp
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+    }
+}
